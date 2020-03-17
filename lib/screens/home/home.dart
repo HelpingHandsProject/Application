c5d9c8d94e295f3711abb760e46ca1f1e252f5d4
Andy <logophoman@gmail.com>
2020-03-17 23:19:08 +0100

    Updated Gitignore to ignore the private Google Key!
    Deleted it from previous pull request -> That one should never be public to ensure the security of the application!

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,48 @@
 import 'package:helping_hands/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
 
   final AuthService _auth = AuthService();
+
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home - Feed',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Map',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: New Request',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Chat',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +50,15 @@ class Home extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
+          //menu bar
+          leading: GestureDetector(
+            onTap: () { /* Write listener code here */ },
+            child: Icon(
+              Icons.menu,  // add custom icons also
+            ),
+          ),
           title: Text('Helping Hands'),
+          centerTitle: true,
           backgroundColor: Colors.green[600],
           elevation: 0.0,
           actions: <Widget>[
@@ -21,8 +68,41 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
+              textColor: Colors.white,
+
             ),
           ],
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              title: Text('Get Help'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              title: Text('Chat'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          unselectedItemColor: Colors.grey[600],
+          onTap: _onItemTapped,
         ),
       ),
     );
