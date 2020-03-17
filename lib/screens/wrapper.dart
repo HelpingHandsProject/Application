ceb2173d749911c4d58bb09ac55f9a2fa15ed651
Andy <logophoman@gmail.com>
2020-03-18 00:16:56 +0100

    gitflow-feature-stash: authentication
    Added: Login, Registering, Logout, Base Screens

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,22 @@
+import 'package:helping_hands/models/user.dart';
+import 'package:helping_hands/screens/authenticate/authenticate.dart';
+import 'package:helping_hands/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
+    
+    // return either the Home or Authenticate widget
+    if (user == null){
+      return Authenticate();
+    } else {
+      return Home();
+    }
+    
+  }
+}
