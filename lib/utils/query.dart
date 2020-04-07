d8be398d2eeb0af25f3eba92d9099d7fbdf8c1e9
Anders Jakob Sivesind <ajsivesind@gmail.com>
2020-04-07 21:05:07 +0200

    refactor: anticipated errors should be handled

--- lib/utils/query.dart
+++ lib/utils/query.dart
@@ -2,9 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<DocumentSnapshot> queryToSingle(Query query) async {
   QuerySnapshot snapshot = await query.getDocuments();
-  try {
+
+  if (snapshot.documents.length > 0) {
     return snapshot.documents.first;
-  } on StateError {
+  } else {
     return null;
   }
 }
